2f1e9361d1709c50369407dbde91bc221eb06ffb
Inaryy <yylovecook@gmail.com>
2023-03-05 23:56:38 +0900

    livewire test

---
+++ resources/views/livewire-test/register.blade.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        <!-- Styles -->
+        @livewireStyles
+    </head>
+    <body>
+        livewireテスト register
+
+        @livewire('register')
+        @livewireScripts
+    </body>
+
+</html>
